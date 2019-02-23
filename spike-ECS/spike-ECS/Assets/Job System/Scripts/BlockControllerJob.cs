ab6990f17d4f499adaefaf5da9130f60bf030bf8
Kurapika <aeonitis@gmail.com>
2019-02-23 19:02:16 +0000

    Implemented the Job system
    - Created a Job
    - Updated the Game Manager

---
+++ spike-ECS/spike-ECS/Assets/Job System/Scripts/BlockControllerJob.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Jobs;
+
+/**
+A job is:
+	- A unit of work that performs a task
+	- Can receive parameters
+	- Operates on data
+	- No reference types
+ */
+
+public struct BlockControllerJob : IJobParallelForTransform
+{
+	public float upperBounds;
+	public float lowerBounds;
+	public float leftBounds;
+	public float rightBounds;
+
+	public float rotateSpeed;
+	public Vector3 rotationDirection;
+
+	public float deltaTime;
+
+    public void Execute(int index, TransformAccess transform)
+    {
+		transform.rotation = transform.rotation * Quaternion.AngleAxis(180 * rotateSpeed * deltaTime, rotationDirection);
+    }
+}
