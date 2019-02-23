5128fe655ef2dd85ea9e37bc719841873b64ab6c
Kurapika <aeonitis@gmail.com>
2019-02-23 17:59:08 +0000

    Classic (non-ECS) implementation of Block spawning added

---
+++ spike-ECS/spike-ECS/Assets/Classic/Scripts/BlockController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+	public float rotateSpeed;
+	public Vector3 rotationDirection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = transform.rotation * Quaternion.AngleAxis(180 * rotateSpeed * Time.deltaTime, rotationDirection);
+	}
+}
