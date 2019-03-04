bef929c04257aec869b519125e6d66d55688c49b
Kurapika <aeonitis@gmail.com>
2019-03-04 23:46:32 +0000

    Set the player to adjust on the sphere surface on collision when triggered
    Changes:
    - Sphere Collider IsTrigger set to true
    - Camera repositioned relative to parent player in Hierarchy
    - Using traditional position change as opposed to addForce on RigidBody
    - Player constraints removed

--- spike-MarioGalaxy/spike-Mario-Galaxy/Assets/PlayerMovement.cs
+++ spike-MarioGalaxy/spike-Mario-Galaxy/Assets/PlayerMovement.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// PlayerMovement assuming spheres
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1.0f;
     public float gravity = 9.98f;
     public Transform gravitySource;
-    private Vector3 gravityVector;
+    /// <param name="gravityVector">Vector pointing inwards. Negative form means pointing outwards.</param>
+    private Vector3 gravityVector;                      
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +23,36 @@ public class PlayerMovement : MonoBehaviour
         // Actual vector between the two positions, normalized to one unit long (to be reused in other code) e.g. multiplied by gravity
         gravityVector = (gravitySource.position - transform.position).normalized * gravity;
 
-        // transform.position +=  gravityVector*Time.deltaTime;
-
         // TODO: Alternative may be to make Z vector look up to handle rotation relative to gravitySource
         transform.rotation = Quaternion.LookRotation(-gravityVector);
 
-        // TODO: Constraints on rotation, set them programatically
-        GetComponent<Rigidbody>().AddForce(gravityVector);
+        transform.position +=  gravityVector*Time.deltaTime;
 
+        // TODO: Constraints on rotation, set them programatically
+        // GetComponent<Rigidbody>().AddForce(gravityVector);
+
+    }
+
+    // To kick you out of the inner body if you sink inwards...
+    // Pre-requisite: Sphere Collider 'Is Trigger' is set to true
+    void OnTriggerEnter(Collider collider) {
+
+        // Cast to gain access to sphere's radius param
+        // SphereCollider sphereCollider = (SphereCollider) collider;
+        // transform.position = gravitySource.position - gravityVector.normalized*sphereCollider.radius;
+        
+        // Set position to as far as the colliding shape's axis radius length (diameter*0.5 for radius) of the vector direction
+        transform.position = gravitySource.position - gravityVector.normalized*collider.transform.localScale.y*0.5f;
+    }
+
+    void OnTriggerStay(Collider collider) {
+
+        // Cast to gain access to sphere's radius param
+        // SphereCollider sphereCollider = (SphereCollider) collider;
+
+        // Set position to as far as the colliding shape's axis radius length (diameter*0.5 for radius) of the vector direction
+        transform.position = gravitySource.position - gravityVector.normalized*collider.transform.localScale.y*0.5f;
+        // transform.position = gravitySource.position - gravityVector.normalized*sphereCollider.radius;
     }
 
     void OnCollisionStay(Collision collision) {
