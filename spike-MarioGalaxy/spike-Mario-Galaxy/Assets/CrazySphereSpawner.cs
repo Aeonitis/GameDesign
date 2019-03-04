3205ed84f73ffd7f309551e654ed9a7fc02aa853
Kurapika <aeonitis@gmail.com>
2019-03-04 22:32:46 +0000

    Starting my base code on newer Unity version 2018.3.6f1 (64-bit)

---
+++ spike-MarioGalaxy/spike-Mario-Galaxy/Assets/CrazySphereSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrazySphereSpawner : MonoBehaviour {
+
+public float spawnDelay = 0.1f;
+public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("Spawn", spawnDelay);
+	}
+
+	void Spawn() {
+		GameObject instance = Instantiate(sphere, transform.position, Quaternion.identity);
+		Rigidbody instanceBody = instance.GetComponent<Rigidbody>();
+		instanceBody.AddForce(Random.onUnitSphere * 1000.0f);
+		instanceBody.GetComponent<Renderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+		Invoke("Spawn", spawnDelay);
+	}
+	
+}
