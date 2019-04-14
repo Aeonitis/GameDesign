a8ca3cf856bb66f2aeaf3b6991396536881e8851
Kurapika <aeonitis@gmail.com>
2019-04-14 14:54:20 +0100

    Camera AI added to independently follow player, no camera orientation logic added yet
    - Added CameraAI script with camera offset following "Player" tagged objects
    - Tagged Mario as "Player"
    - Extracted camera from Hierarchy

---
+++ spike-MarioGalaxy/spike-Mario-Galaxy/Assets/Scripts/CameraAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAI : MonoBehaviour
+{
+
+    public string tag = "Player";
+    private Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Getting player position
+        Transform player = GameObject.FindGameObjectWithTag(tag).transform;
+        // Getting camera offset, representing Vector between player and camera, a distance
+        cameraOffset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform player = GameObject.FindGameObjectWithTag(tag).transform;
+        
+        transform.position = player.position + cameraOffset;
+    }
+}
